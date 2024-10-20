2032241c6eec767d0b8334668697310db365b5eb
anishazahan <anishazahan13@gmail.com>
2024-10-20 07:33:57 +0600

    model create and get all courses

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,12 @@
+import { getCourses } from "@/BackendService/queries/courses";
 import Test from "@/components/Test";
-import { Button } from "@/components/ui/button";
-import { Card } from "@/components/ui/card";
 
-export default function Home() {
-  return (
-    <div className="text-red-600">
-      hellow
-      <Button>Button</Button>
-      <Card>heloww</Card>
-      <Test />
-    </div>
-  );
+export default async function Home() {
+  const courses = await getCourses();
+  console.log(courses);
+
+  console.log(courses[0]?.instructor?.socialMedia);
+  console.log(courses[0]?.testimonials);
+  console.log(courses[0]?.modules);
+  return <Test />;
 }
