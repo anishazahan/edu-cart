7b66062aec518bcdac696ea54a8d06eadaa079c4
anishazahan <anishazahan13@gmail.com>
2024-10-20 08:38:50 +0600

    add menu and hero section styling

---
+++ src/components/custom/logo.jsx
@@ -0,0 +1,8 @@
+export const Logo = ({ className = "" }) => {
+  return (
+    <div className="">
+      {/* <Image className={cn("max-w-[100px]", className)} src={logo} alt="logo" /> */}
+      <h2 className="text-xl text-green-600 font-semibold">Logo</h2>{" "}
+    </div>
+  );
+};
