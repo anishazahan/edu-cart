2b113dd3dfaa5a65fadac60ca5e00a650851b318
anishazahan <anishazahan13@gmail.com>
2024-10-24 10:05:19 +0600

    login with google auth provider

---
+++ src/app/login/_components/social-logins.jsx
@@ -0,0 +1,27 @@
+import { doSocialLogin } from "@/app/actions";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import img from "../../../public/google.png";
+
+const SocialLogins = () => {
+  return (
+    <>
+      <div className="text-center text-md mt-3 text-gray-500">or Signup with</div>
+      <form action={doSocialLogin}>
+        <div className="flex justify-center gap-2">
+          <Button
+            className="mt-4 py-2 border-gray-600/30 border rounded-md flex items-center gap-2 justify-center"
+            type="submit"
+            name="action"
+            value="google"
+          >
+            <Image src={img} alt="google" width={20} height={20} />
+            <span>Google</span>
+          </Button>
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default SocialLogins;
