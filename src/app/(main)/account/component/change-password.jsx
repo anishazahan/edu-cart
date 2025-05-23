53ba53edbe2614ba7717c5831aa1e815edfcedc6
anishazahan <anishazahan13@gmail.com>
2025-05-23 22:59:08 +0600

    path directory adjust

--- src/app/(main)/account/component/change-password.jsx
+++ src/app/(main)/account/component/change-password.jsx
@@ -1,13 +1,12 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-
 import { useState } from "react";
 import { toast } from "sonner";
 
 import { changePassword } from "@/app/actions/account";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 const ChangePassword = ({ email }) => {
   const [passwordState, setPasswordState] = useState({
